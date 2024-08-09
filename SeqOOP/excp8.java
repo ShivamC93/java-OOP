f01487b7c09f1e538d8ba3b1e8911366aa835fe6
Shivam Chaugule <114950843+ShivamC93@users.noreply.github.com>
2024-08-09 22:33:10 +0530

    Add files via upload

---
+++ SeqOOP/excp8.java
@@ -0,0 +1,13 @@
+//throws
+
+public class excp8 {
+    public static void Wait() throws InterruptedException{
+        for(int i=0;i<10;i++){
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
+    }
+    public static void main(String[] args) throws InterruptedException {
+        Wait();
+    }
+}
